9345719571f8c9cf12322c4f76ee340567256888
ilesyk <pavloyavorskyi@ukr.net>
2023-07-21 23:56:58 +0300

    hw11 v2

--- src/index.js
+++ src/index.js
@@ -7,54 +7,52 @@ const API_KEY = '38386406-5744ae5dca70e3cf5c44041b1';
 const selectors = {
   form: document.querySelector('#search-form'),
   div: document.querySelector('.gallery'),
-  load: document.querySelector('.load-more'),
   sentinel: document.querySelector('.js-sentinel'),
 };
 
 selectors.form.addEventListener('submit', onSubmit);
-// selectors.load.addEventListener('click', onLoadMore);
+
+let searchQ = '';
+let currentPage = 1;
+let allImages = [];
 
 function onSubmit(evt) {
   evt.preventDefault();
   selectors.div.innerHTML = '';
   if (selectors.form.elements.searchQuery.value) {
-    const q = selectors.form.elements.searchQuery.value;
-    console.log(q);
-    serviceImages(q)
+    searchQ = selectors.form.elements.searchQuery.value;
+    currentPage = 1;
+    serviceImages(searchQ, currentPage)
       .then(data => {
         if (data.hits.length > 0) {
-          createMarkup(data);
-          // selectors.load.hidden = false;
+          allImages = data.hits;
+          createMarkup(allImages);
           observer.observe(selectors.sentinel);
           Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
         } else {
           Notiflix.Notify.failure(
             'Sorry, there are no images matching your search query. Please try again.'
           );
-          selectors.load.hidden = true;
         }
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+        Notiflix.Notify.failure(
+          'Failed to fetch images. Please try again later.'
+        );
+      });
   }
 }
 
-let page = 1;
-
-// function onLoadMore() {
-//   page += 1;
-//   serviceImages(page).then(data => {
-//     createMarkup(data);
-//   });
-// }
-async function serviceImages(q, page = 1) {
+async function serviceImages(q, page) {
   const resp = await axios.get(
     `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
   );
   return resp.data;
 }
 
-function createMarkup(data) {
-  const markup = data.hits
+function createMarkup(images) {
+  const markup = images
     .map(
       ({
         webformatURL,
@@ -95,10 +93,26 @@ let observer = new IntersectionObserver(handlerScroll, options);
 function handlerScroll(entries, observer) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      page += 1;
-      serviceImages(page).then(data => {
-        createMarkup(data);
-      });
+      currentPage += 1;
+      serviceImages(searchQ, currentPage)
+        .then(({ hits, totalHits }) => {
+          allImages.push(...hits);
+          createMarkup(hits);
+            console.log(allImages);
+            if (allImages.length >= totalHits) {
+                observer.unobserve(entry.target);
+                Notiflix.Notify.info(
+                  "We're sorry, but you've reached the end of search results."
+                );
+            }
+        })
+        .catch(err => {
+          console.log(err);
+          observer.unobserve(entry.target);
+          Notiflix.Notify.failure(
+            'Failed to fetch images. Please try again later.'
+          );
+        });
     }
   });
 }
